c1c1120ba0188712e8accb40d0d202adca94b761
Simcha Greenbaum <gbs4ever2@gmail.com>
2020-01-23 23:12:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your functions here
-function map(){
-
+function map(arr,func){
+let arr= []
+for( let i=0;arr>arr.length;i++){
+let elment = arr[i]  
+ arr.push(func(elment))
+}
+return arr
 }
